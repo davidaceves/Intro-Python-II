914d1f80ec734e90954844d38a57cfedc6b8c50b
David <digital0david@gmail.com>
2019-11-22 22:38:07 -0500

    removed _to

--- src/room.py
+++ src/room.py
@@ -2,12 +2,12 @@
 # description attributes.
 
 class Room: 
-    def __init__(self, name, description, n_to=None, s_to=None, e_to=None, w_to=None):
+    def __init__(self, name, description):
         self.name = name
         self.description = description
-        self.n_to = n_to
-        self.s_to = s_to
-        self.e_to = e_to
-        self.w_to = w_to
+   
+       
+
+
     def __str__(self):
         return f'Current Room: {self.name} \nDescription: {self.description}'
