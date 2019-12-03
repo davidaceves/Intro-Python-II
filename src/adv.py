442558ac005b1654c01399d87744a2dadbba4f29
David <digital0david@gmail.com>
2019-12-02 22:05:01 -0500

    Item class added to rooms, takeItem func

--- src/adv.py
+++ src/adv.py
@@ -1,25 +1,27 @@
 from room import Room
 from player import Player
+from item import Item
+from item import Gold
 
 # Declare all the rooms
 
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons", ["Broken sword", "1 gold"]),
+                     "North of you, the cave mount beckons", [Item("Axe", "A dull axe"), Gold(10)]),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east.""", ["Candle"]),
+passages run north and east.""", [Item("Wand", "An old wand with strange runes" )]),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm.""", ["Strange Amulet"]),
+the distance, but there is no way across the chasm.""", [Item("Skull", "A pale white skull of some strange monster")]),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air.""", ["Skull"]),
+to north. The smell of gold permeates the air.""", [Item("Key", "A bent key")]),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south.""", ["Faded Map"]),
+earlier adventurers. The only exit is to the south.""", [Item("Map", "A faded map")]),
 }
 
 
@@ -40,7 +42,7 @@ room['treasure'].s_to = room['narrow']
 
 # Make a new player object that is currently in the 'outside' room.
 
-newPlayer = Player("David", room['outside'], ["short sword"])
+newPlayer = Player("David", room['outside'])
 
 # Write a loop that:
 #
@@ -64,20 +66,45 @@ def movePlayer(direction):
           
         else: 
             print("Try somewhere else...")
+    print(newPlayer.inventory)
 
+def takeItem(item):
+    # get_item = "get" + items
 
-print(newPlayer.current_room)
+    if item == "pass":
+        return
 
+    # if selectItem == get_item:
+    for items in newPlayer.current_room.items:
+        if(items.name == item):
+            print(newPlayer.current_room.items)
+
+            newPlayer.inventory.append(items)
+            newPlayer.current_room.items.remove(items)
+
+            print(f'Inventory: {newPlayer.inventory}')
+            print(f'Room in Items: {newPlayer.current_room.items}')
+        else: 
+            print("Item does not exist or invalid command")
+
+    
+# for items in newPlayer.current_room.items:
+#     print(f'Items: {items}')
+    
+
+# print(newPlayer.current_room.items)
+# print(newPlayer.current_room)
 
 while True:
     selectItem = input("Take item: ")
-    selectDirection = input("Choose a direction: ")
+    # selectDirection = input("Choose a direction: ")
 
-    if (selectDirection == "q"):
-        print("Goodbye.")
-        break
+    # if (selectDirection == "q"):
+    #     print("Goodbye.")
+    #     break
 
-    movePlayer(selectDirection)
+    takeItem(selectItem)
+    # movePlayer(selectDirection)
 
    
 
